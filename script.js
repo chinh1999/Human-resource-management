8644472811fdcf36d59c827fd41dbc31ca319c79
Chính <chinh19990919@gmail.com>
2020-03-10 13:16:13 +0700

    Fix again index.html and style.css

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ function login() {
 	var pass = document.getElementById("matkhau").value;
 	if(id == "admin" && pass == "admin"){
 		window.location.href = 'file:///C:/Users/ADMIN/Desktop/TTN/index.html';
-		document.getElementById("label").innerHTML = 'Hi';
 	}
 	else{
-		alert("Sai tên đăng nhập hoặc mật khẩu, mời thử lại!!")
+		alert("Sai tên đăng nhập hoặc mật khẩu, mời thử lại!!");
 	}
 }
+
+function Changecolor(count) {
+	for(var x=0; x<=4; x++){
+		document.getElementsByClassName("inner-menu")[x].style.backgroundColor = 'white';
+	}
+	document.getElementsByClassName("inner-menu")[count].style.backgroundColor = '#ffae00';
+}
+
